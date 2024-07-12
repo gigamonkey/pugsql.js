c1f4f6f25c65ef7e47ec69d627ec2b8b5ee69ee2
Peter Seibel <peter@gigamonkeys.com>
2024-07-12 10:09:59 -0700

    Sort objects by tbl_name in puglify output.

--- data/puglify.sql
+++ data/puglify.sql
@@ -1,11 +1,11 @@
 -- :name allObjects :all
-select * from sqlite_schema
+select * from sqlite_schema order by tbl_name;
 
 -- :name tableInfo :all
 select * from pragma_table_info($table);
 
 -- :name columns :list
-select name from pragma_table_info($table)
+select name from pragma_table_info($table);
 
 -- :name primaryKeys :list
 select name from pragma_table_info($table) where pk = 1;
